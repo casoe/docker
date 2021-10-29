20929c3725af2b9a405f6613b124b20fc15a44a4
Carsten Söhrens <casoe@gmx.de>
2021-10-30 00:03:18 +0200

    Postgres für FHEM hinzugefügt

---
+++ postgres_fhem.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT="postgres_fhem"
+USER="fhem"
+
+mkdir -p ~/docker/data/$SCRIPT
+docker run -d \
+  --name=postgres \
+  --restart=always \
+  -p 5432:5432 \
+  -v ~/docker/$SCRIPT/data:/var/lib/postgresql/data \
+  -e POSTGRES_USER=$USER \
+  -e POSTGRES_PASSWORD=$USER \
+  -e POSTGRES_DB=$USER \
+  postgres:14-alpine \
+  -c shared_buffers=256MB \
+  -c effective_cache_size=512MB
