bdb6d236db6e12586a83a5521ecbb7150fb32a51
Carsten Söhrens <casoe@gmx.de>
2024-06-23 16:48:48 +0200

    Umstellung DokuWiki auf PHP-Container

--- dokuwiki.sh
+++ dokuwiki.sh
@@ -8,6 +8,15 @@ docker run -d \
   -e PGID=1000 \
   -e TZ=Europe/London \
   -p 81:80 \
-  -v ~/docker/data/dokuwiki:/config \
+  -v ~/docker/data/dokuwiki:/var/www/html \
   --restart unless-stopped \
-  lscr.io/linuxserver/dokuwiki:latest
+  php:7-apache-bullseye
+
+echo https://loganmarchione.com/2022/03/the-best-way-to-run-dokuwiki-in-docker/
+echo docker exec -it dokuwiki /bin/bash
+echo cd /var/www/html
+echo curl --remote-name https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
+echo tar -xzvf dokuwiki-stable.tgz --strip-components=1
+echo rm dokuwiki-stable.tgz
+echo chown -R www-data:www-data /var/www/
+
