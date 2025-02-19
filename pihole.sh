4c19f5be35c3e24d63724ed2b166fe9e19e825a7
Carsten Söhrens <casoe@gmx.de>
2025-02-19 21:28:05 +0100

    Umstellung auf Pi-hole 6

--- pihole.sh
+++ pihole.sh
@@ -18,29 +18,25 @@ echo My ip address: $MY_IP
 
 PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 
-# Note: ServerIP should be replaced with your external ip.
 docker run -d \
     --name pihole \
     --restart=unless-stopped \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -e TZ="Europe/Berlin" \
-    -e FTLCONF_REPLY_ADDR4=$MY_IP \
+    -e FTLCONF_webserver_api_password="" \
+    -e FTLCONF_dns_listeningMode=all \
     -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-    --dns=$MY_IP --dns=1.1.1.1 \
     --hostname $MY_HOSTNAME \
-    -e VIRTUAL_HOST=$MY_HOSTNAME \
-    -e PROXY_LOCATION=$MY_HOSTNAME \
-    -e ServerIP=$MY_IP \
+    --cap-add NET_ADMIN \
     pihole/pihole:latest
 
 printf 'Starting up pihole container '
 for i in $(seq 1 20); do
     if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
         printf ' OK'
-        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: https://${IP}/admin/"
-        exit 0
+	exit 0
     else
         sleep 3
         printf '.'
@@ -53,8 +49,8 @@ for i in $(seq 1 20); do
 done;
 
 echo "Removing password by"
-echo docker exec pihole /bin/bash -c "echo -ne '\n' | pihole -a -p"
-docker exec pihole /bin/bash -c "echo -ne '\n' | pihole -a -p"
+echo docker exec pihole /bin/bash -c "echo -ne '\n' | sudo pihole setpassword"
+docker exec pihole /bin/bash -c "echo -ne '\n' | sudo pihole setpassword"
 
 echo
 echo "How to create backup via crontab"
